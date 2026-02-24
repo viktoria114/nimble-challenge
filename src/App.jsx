44b305fe7dcd0d1612167c1c691a5de425e68c77
viktoria114 <90705329+viktoria114@users.noreply.github.com>
2026-02-24 14:39:13 -0300

    basic interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,72 @@
-import './App.css'
+import "./App.css";
+import {
+  Button,
+  InputAdornment,
+  Paper,
+  TextField,
+  Typography,
+  IconButton,
+  Box,
+} from "@mui/material";
+import SendIcon from '@mui/icons-material/Send';
+import { useState } from "react";
 
 function App() {
+const [email, setEmail] = useState("");
+const [loading, setLoading] = useState(false);
 
+const handleSubmit = () => {
+  setLoading(true);
+  // Aquí puedes agregar la lógica para enviar el correo electrónico al backend
+}
+  
   return (
     <>
-      <div>
-      <h1>Nimble Gravity Challenge</h1>
-    </div>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <Typography variant="h4" sx={{ color: "#000", fontWeight: "bold", fontFamily: "Poppins, sans-serif" }}>
+          Nimble Gravity Challenge
+        </Typography>
+
+        <Paper
+          elevation={3}
+          sx={{
+            p: 4,
+            borderRadius: 2,
+            bgcolor: "#c69fd5",
+           m: 2,
+            display: "flex",
+            alignItems: "center",
+            mt: 4,
+          }}
+        >
+          <Typography variant="h6" sx={{ color: "#000", fontWeight: "400",  fontFamily: "Poppins, sans-serif"  }}>
+            Ingresa tu correo electrónico!
+          </Typography>{" "}
+          <TextField
+          value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            variant="outlined"
+            size="small"
+            sx={{ width: 360, ml: 2, bgcolor: "#fff", borderRadius: 2,  fontFamily: "Poppins, sans-serif"  }}
+          ></TextField>
+          <Button variant="contained"  onClick={handleSubmit}
+            loading={loading} sx={{ ml: 2, bgcolor: "#000", color: "#fff", "&:hover": { bgcolor: "#6e5877ff", } }}>
+            
+            <SendIcon  />
+          </Button>
+        </Paper>
+      </Box>
+      <div></div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
